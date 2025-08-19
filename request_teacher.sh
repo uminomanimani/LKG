a287d951ddf7a6377a35187bb1ced112708d2251
海のまにまに <42465598+uminomanimani@users.noreply.github.com>
2025-08-19 08:53:18 +0800

    Update request_teacher.sh

--- request_teacher.sh
+++ request_teacher.sh
@@ -4,24 +4,24 @@ set -e
 teacher_model="gpt-4o-mini"
 dataset="docred"
 n_threads=16
-dataset_type=("train" "dev" "test")
+dataset_types=("train" "dev" "test")
 
-for _dataset_type in "${dataset_type[@]}"; do
+for dataset_type in "${dataset_types[@]}"; do
     # request entity descriptions via teacher model, to which the entities are visible
     python request_entity_description.py \
         --teacher_model "${teacher_model}" \
         --dataset "${dataset}" \
         --n_threads "${n_threads}" \
-        --dataset_type "${_dataset_type}"
+        --dataset_type "${dataset_type}"
 
     # generate alpaca format for supervised fine-tuning
     python ner_sft.py \
         --teacher_model "${teacher_model}" \
         --dataset "${dataset}" \
-        --dataset_type "${_dataset_type}"
+        --dataset_type "${dataset_type}"
     
     python build_graph_for_teacher.py \
         --teacher_model "${teacher_model}" \
         --dataset "${dataset}" \
-        --dataset_type "${_dataset_type}"
+        --dataset_type "${dataset_type}"
 done
